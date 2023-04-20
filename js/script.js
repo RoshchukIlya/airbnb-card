81c96bd8c99eb70960921eb92173626aa709c2a3
Ilya <Roshchukilya@gmail.com>
2023-04-20 18:56:33 +0600

    [PM-#0.3]implement layout js

---
+++ js/script.js
@@ -0,0 +1,78 @@
+import { Header } from './header.js';
+// Точка входа в app
+const createApp = () => {
+  const header = Header();
+  document.body.append(header);
+}
+// Запуск app
+createApp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// С Арсеном делали:
+
+// // document.body.style.background = 'red'; // меняет background
+// // setTimeout(() => document.body.style.background = 'green', 2000); // меняет background со временем
+// // setInterval(() => document.body.style.background = 'blue', 4000); // меняет background со временем, но меняется через интевал
+// // setInterval(() => document.body.style.background = 'yellow', 6000);
+
+// const form = document.querySelector('#form');
+// const nameInp = form.querySelector('#name');
+
+// console.log(form);
+// console.log(nameInp);
+
+// nameInp.addEventListener('change', (event) => {
+//   console.log(event.target.value);
+// })
+
+// let obj = {
+//   mane: '',
+//   airbnbURL: '',
+//   imgURL: '',
+//   price: 0,
+//   summary: '',
+// }
+
+// let formHandler = () => {
+
+// };
+
+// let newCard = (imgURL, name, summary, airbnbURL, price) => {
+//   return `
+//   <div class="chank">
+//           <img src="${imgURL}" alt="house">
+//           <h2 class="chank-title">${name}</h2>
+//           <div class="chank-summary">
+//             <h3 class="summary-title">Summary</h3>
+//             <p class="summary-description">${summary}</p>
+//           </div>
+//           <div class="chank-references">
+//             <h3 class="references-title">References</h3>
+//             <ul class="references-list">
+//               <li class="references-item">
+                // <a href="${airbnbURL}">👉🏿 airbnb</a>
+//               </li>
+//               <li class="references-item">$${price} night</li>
+//             </ul>
+//           </div>
+//         </div>`
+// }
